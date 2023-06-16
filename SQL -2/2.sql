84b8771dedd4cd59a450ecda76fb2f91a10b87aa
Куртов Константин Дмитриевич <kurtov@yst.local>
2023-06-16 16:18:28 +0300

    Задание 12-3-2-2 выполнено

--- SQL -2/2.sql
+++ SQL -2/2.sql
@@ -21,3 +21,48 @@ SELECT * FROM vegetables_and_fruits WHERE type = 'овощ' AND name LIKE '%Ка
 SELECT * FROM vegetables_and_fruits WHERE description LIKE '%Картофель%';
 ■ Показать все овощи и фрукты, у которых цвет зеленый или оранжевый.
 SELECT * FROM vegetables_and_fruits WHERE color IN ('зеленый', 'оранжевый');
+Задание 2
+Создайте следующие запросы для базы данных с
+информацией об овощах и фруктах из предыдущего домашнего задания:
+■ Показать количество овощей;
+SELECT COUNT(*) FROM vegetables_and_fruits WHERE type = 'овощ';
+■ Показать количество фруктов;
+SELECT COUNT(*) FROM vegetables_and_fruits WHERE type = 'фрукт';
+■ Показать количество овощей и фруктов заданного цвета;
+SELECT type, color, COUNT(*)
+FROM vegetables_and_fruits
+WHERE color = 'оранжевый'
+GROUP BY type, color;
+■ Показать количество овощей ифруктов каждого цвета;
+SELECT type, color, COUNT(*)
+FROM vegetables_and_fruits
+GROUP BY type, color;
+■ Показать цвет с минимальным количеством овощей и фруктов;
+SELECT color, COUNT(*) AS count
+FROM vegetables_and_fruits
+GROUP BY color
+ORDER BY count ASC
+LIMIT 1;
+■ Показать цвет с максимальным количеством овощей и фруктов;
+SELECT color, COUNT(*) AS count
+FROM vegetables_and_fruits
+GROUP BY color
+ORDER BY count DESC
+LIMIT 1;
+■ Показать минимальную калорийность овощей ифруктов;
+SELECT MIN(calories) AS min_calories
+FROM vegetables_and_fruits;
+■ Показать максимальную калорийность овощей и фруктов;
+SELECT MAX(calories) AS max_calories
+FROM vegetables_and_fruits;
+■ Показать среднюю калорийность овощей и фруктов;
+SELECT AVG(calories) AS avg_calories
+FROM vegetables_and_fruits;
+■ Показать фрукт с минимальной калорийностью;
+SELECT name, MIN(calories) AS min_calories
+FROM vegetables_and_fruits
+WHERE type = 'фрукт';
+■ Показать фрукт с максимальной калорийностью
+SELECT name, MAX(calories) AS max_calories
+FROM vegetables_and_fruits
+WHERE type = 'фрукт';
